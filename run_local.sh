d05ec5ab405c11c7c6b8b876c030dd4f0f1ffa3c
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2024-08-15 14:55:58 +0100

    APIS-7255 PR actions

--- run_local.sh
+++ run_local.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sbt "~run -Drun.mode=Dev -Dhttp.port=9614 -Dapplication.router=testOnlyDoNotUseInAppConf.Routes $*"
+sbt "~run -Drun.mode=Dev -Dhttp.port=9614 -Dapplication.router=testOnlyDoNotUseInAppConf.Routes -Ddeskpro.api-key=${DESKPRO_KEY} $*"
