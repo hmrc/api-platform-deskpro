080f2a7faa9208636f5b8fcf1c00d782b2efb7ca
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2024-08-29 13:23:49 +0100

    added job enabled flag so we can disable by default

--- test/uk/gov/hmrc/apiplatformdeskpro/service/OrganisationServiceSpec.scala
+++ test/uk/gov/hmrc/apiplatformdeskpro/service/OrganisationServiceSpec.scala
@@ -18,8 +18,15 @@ package uk.gov.hmrc.apiplatformdeskpro.service
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+
 import uk.gov.hmrc.apiplatformdeskpro.connector.DeskproConnector
-import uk.gov.hmrc.apiplatformdeskpro.domain.models.connector.{DeskproLinkedObject, DeskproOrganisationResponse, DeskproOrganisationWrapperResponse, DeskproPersonResponse, DeskproResponse}
+import uk.gov.hmrc.apiplatformdeskpro.domain.models.connector.{
+  DeskproLinkedObject,
+  DeskproOrganisationResponse,
+  DeskproOrganisationWrapperResponse,
+  DeskproPersonResponse,
+  DeskproResponse
+}
 import uk.gov.hmrc.apiplatformdeskpro.domain.models.{DeskproOrganisation, DeskproPerson, OrganisationId}
 import uk.gov.hmrc.apiplatformdeskpro.utils.AsyncHmrcSpec
 import uk.gov.hmrc.http.{HeaderCarrier, UpstreamErrorResponse}
