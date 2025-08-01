04a57902bbbfad459441bdcd9b71d745c1d04f29
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2025-08-01 13:58:35 +0100

    APIS-7732 PR bot actions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,8 +5,8 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.16.0"
-  private val hmrcMongoVersion = "2.6.0"
+  private val bootstrapVersion = "9.18.0"
+  private val hmrcMongoVersion = "2.7.0"
   private val commonDomainVersion = "0.18.0"
 
   val compile = Seq(
